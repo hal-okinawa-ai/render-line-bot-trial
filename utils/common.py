fc98049c17ffa59e5c45940af95a63db274da088
hal-okinawa-ai <hal.okinawa.ai@gmail.com>
2025-03-24 16:01:03 +0900

    Update common.py

--- utils/common.py
+++ utils/common.py
@@ -1,5 +1,11 @@
 import random
 import string
+from datetime import datetime
+import pytz
 
 def generate_referral_code():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+
+def get_japan_time():
+    tz = pytz.timezone('Asia/Tokyo')
+    return datetime.now(tz)
